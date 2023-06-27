b1bd6b7fea4d3fab279720a816705e7a3ab065ee
orion-orion <orion-orion@foxmail.com>
2023-06-27 17:41:34 +0800

    feat: add fedprox in multiprocess

--- fed_multiprocess_syn/client.py
+++ fed_multiprocess_syn/client.py
@@ -1,37 +1,56 @@
+# -*- coding: utf-8 -*-
 import gc
 import torch
 from torch.utils.data import DataLoader
 from torch.utils.data.dataloader import default_collate
 
+
 class Client:
-    def __init__(self, model_fn, optimizer_fn, args, train_dataset, test_dataset, valid_dataset):
+    def __init__(self, model_fn, optimizer_fn, args, train_dataset,
+                 test_dataset, valid_dataset):
         self.device = "cuda:%s" % args.gpu if args.cuda else "cpu"
         self.model = model_fn().to(self.device)
         self.optimizer = optimizer_fn(self.model.parameters())
-        
-        self.train_dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, \
-                collate_fn=self.collate_fn)
-        self.valid_dataloader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False, \
-                collate_fn=self.collate_fn)
-        self.test_dataloader = DataLoader(test_dataset, batch_size=args.batch_size, shuffle=False, \
-                collate_fn=self.collate_fn)
 
-        # 此处为传引用，model参数变化self.weights而变化，get_weights()函数也实际返回的weights的引用
-        self.weights = {key : value for key, value in self.model.named_parameters()}
-        
-        # 初始化各client的样本占所有样本的比率
+        # Here `collate_fn` is used to move data to `self.device`
+        self.train_dataloader = DataLoader(train_dataset,
+                                           batch_size=args.batch_size,
+                                           shuffle=True,
+                                           collate_fn=self.collate_fn)
+        # Here `collate_fn` is used to move data to `self.device`
+        self.valid_dataloader = DataLoader(valid_dataset,
+                                           batch_size=args.batch_size,
+                                           shuffle=False,
+                                           collate_fn=self.collate_fn)
+        # Here `collate_fn` is used to move data to `self.device`
+        self.test_dataloader = DataLoader(test_dataset,
+                                          batch_size=args.batch_size,
+                                          shuffle=False,
+                                          collate_fn=self.collate_fn)
+
+        # Here `self.weights` is a reference of the model parameters, and the
+        # parameters of the model change with `self.weights`
+        self.weights = {key: value for key,
+                        value in self.model.named_parameters()}
+
+        # Initialize the proportion of the samples of each client to
+        # all samples
         self.train_pop, self.valid_prop, self.test_prop = 0.0, 0.0, 0.0
-        
-        # 初始化各client的权重
+
+        # Compute the number of samples for each client
         self.n_samples_train = len(train_dataset)
         if args.valid_frac > 0:
             self.n_samples_valid = len(valid_dataset)
         self.n_samples_test = len(test_dataset)
-        
-    def train_epoch(self, c_id, train_logs, args):
-        """
-            Train one client with its own data for one epoch
-            cid: Client id
+
+    def train_epoch(self, c_id, train_logs, args, global_weights):
+        """Train one client with its own data for one epoch.
+
+        Args:
+            c_id: client ID.
+            train_logs: training logs.
+            args: other parameters for training.
+            global_weights: global model weights used in `FedProx` method.
         """
         torch.manual_seed(args.seed + c_id)
         self.model.train()
@@ -40,24 +59,52 @@ class Client:
             for x, y in self.train_dataloader:
                 # x, y = x.to(self.device), y.to(self.device)
                 self.optimizer.zero_grad()
-                # Pytorch自带softmax和默认取了mean
+
+                # The `CrossEntropyLoss` function in Pytorch will divide by the
+                # mini-batch size by default
                 l = torch.nn.CrossEntropyLoss()(self.model(x), y)
+                if args.fed_method == "FedProx":
+                    l += self.prox_reg(dict(self.model.named_parameters()),
+                                       global_weights, args.mu)
+
                 l.backward()
-                self.optimizer.step()  
-                
+                self.optimizer.step()
+
                 loss += l.item() * y.shape[0]
                 n_samples += y.shape[0]
-                
+
             gc.collect()
         train_logs[c_id] = float(loss / n_samples)
 
-    def evaluation(self, c_id, random_clients, eval_logs, mod="valid"):   
-        if c_id not in random_clients and mod == "valid":
-                eval_logs[c_id] = self.get_old_eval_log()
+    @ staticmethod
+    def flatten(params):
+        return torch.cat([param.flatten() for param in params])
+
+    def prox_reg(self, params1, params2, mu):
+        # The parameters returned by `named_parameters()` should be rearranged
+        # according to the keys of the parameters returned by `state_dict()`
+        params2_values = [params2[key] for key in params1.keys()]
+
+        # Multi-dimensional parameters should be flattened into one-dimensional
+        vec1 = self.flatten(params1.values())
+        vec2 = self.flatten(params2_values)
+        return mu / 2 * torch.norm(vec1 - vec2)**2
+
+    def evaluation(self, c_id, random_clients, eval_logs, mode="valid"):
+        """Evaluation one client with its own data for one epoch.
+
+        Args:
+            c_id: client ID.
+            random_clients: randomly selected clients.
+            eval_logs: evaluation logs.
+            mode: choose valid or test mode.
+        """
+        if c_id not in random_clients and mode == "valid":
+            eval_logs[c_id] = self.get_old_eval_log()
         else:
-            if mod == "valid":
+            if mode == "valid":
                 dataloader = self.valid_dataloader
-            elif mod == "test":
+            elif mode == "test":
                 dataloader = self.test_dataloader
 
             self.model.eval()
@@ -65,7 +112,7 @@ class Client:
             with torch.no_grad():
                 for x, y in dataloader:
                     # x, y = x.to(self.device), y.to(self.device)
-                    
+
                     y_hat = self.model(x)
                     pred = torch.argmax(y_hat.data, 1)
 
@@ -74,22 +121,29 @@ class Client:
 
             gc.collect()
             self.acc = correct/n_samples
-            if mod == "valid":
+            if mode == "valid":
                 eval_logs[c_id] = float(self.acc * self.valid_prop)
-            elif mod == "test":
+            elif mode == "test":
                 eval_logs[c_id] = float(self.acc * self.test_prop)
-                     
+
     def get_old_eval_log(self):
+        """Returns the evaluation result of the lastest epoch.
+        """
         return self.acc
 
     def get_weights(self):
-        """ Return all of the weights list """
+        """Returns all of the weights in `dict` format. Note that
+        `self.weights` is a reference of the model parameters.
+        """
         return self.weights
 
     def set_global_weights(self, global_weights):
-        """ Assign all of the weights with global weights """
+        """Assigns all of the weights with global weights.
+        """
         for name in self.weights.keys():
             self.weights[name].data = global_weights[name].data.clone()
 
     def collate_fn(self, batch):
+        """Moves data to `self.device`.
+        """
         return tuple(x.to(self.device) for x in default_collate(batch))
--- fed_multiprocess_syn/fl.py
+++ fed_multiprocess_syn/fl.py
@@ -1,88 +1,96 @@
-import logging
-import torch.multiprocessing as mp   
-import torch
+# -*- coding: utf-8 -*-
 import time
+import logging
+import torch
+import torch.multiprocessing as mp
 
 
 def training_logging(train_logs, epoch, args):
     for c_id, train_val in enumerate(train_logs):
-        logging.info('Global epoch {}/{} - client {} -  Training Loss: {:.3f}'.format(epoch, args.global_epochs, c_id, train_val))
+        logging.info("Global epoch {}/{} - client {} -  Training Loss: {:.3f}".format(
+            epoch, args.global_epochs, c_id, train_val))
 
 
-def evaluation_logging(eval_logs, epoch, mod="valid"):
-    if mod == "valid":
-        logging.info('Global epoch%d Valid:' % epoch)
+def evaluation_logging(eval_logs, epoch, mode="valid"):
+    if mode == "valid":
+        logging.info("Global epoch%d Valid:" % epoch)
     else:
-        logging.info('Test:')
-        
+        logging.info("Test:")
+
     avg_eval_val = 0.0
     for eval_val in eval_logs:
         avg_eval_val += eval_val.data
-    
-    logging.info('ACC: ' + str(avg_eval_val))
-       
-       
+
+    logging.info("ACC: " + str(avg_eval_val))
+
+
 def run_fl(clients, server, args):
-    mp.set_start_method('spawn', force=True)
-    
+    mp.set_start_method("spawn", force=True)
+
     begin = time.time()
     # Train with these clients
     for epoch in range(1, args.global_epochs + 1):
-
+        # Choose some clients that will train on this epoch
         random_clients = server.choose_clients(args.n_clients, args.frac)
-      
+
         # Train with these clients
+        # Restore global weights to client's model
+        global_weights = server.get_global_weights()
         processes = []
-        train_logs = torch.tensor([0.0 for _ in range(args.n_clients)]).share_memory_()
+        train_logs = torch.tensor(
+            [0.0 for _ in range(args.n_clients)]).share_memory_()
         for c_id in random_clients:
             # Restore global weights to client's model
-            clients[c_id].set_global_weights(server.get_global_weights())
-            
-            p = mp.Process(target=clients[c_id].train_epoch, args=(c_id, train_logs, args))
-            # We train the model across `num_processes` processes
+            clients[c_id].set_global_weights(global_weights)
+
+            p = mp.Process(target=clients[c_id].train_epoch, args=(
+                c_id, train_logs, args, global_weights))
+            # Train the model across `num_processes` processes
             p.start()
             processes.append(p)
-                        
+
         for p in processes:
             p.join()
         training_logging(list(train_logs), epoch, args)
-    
-        # We are going to sum up active clients' weights at each epoch
+
+        # Sum up active clients' weights at each epoch
         server.aggregate_weights([clients[c_id] for c_id in random_clients])
 
         # Evaluation on valid dataset
         if epoch % args.eval_interval == 0:
-            eval_logs = torch.tensor([0.0 for _ in range(args.n_clients)]).share_memory_()
+            eval_logs = torch.tensor(
+                [0.0 for _ in range(args.n_clients)]).share_memory_()
             processes = []
             for c_id in range(args.n_clients):
                 # Restore global weights to client's model
                 clients[c_id].set_global_weights(server.get_global_weights())
-                p = mp.Process(target=clients[c_id].evaluation, args=(c_id, random_clients, eval_logs, "valid"))
-                # We evaluate the model across `num_processes` processes
+                p = mp.Process(target=clients[c_id].evaluation, args=(
+                    c_id, random_clients, eval_logs, "valid"))
+                # Evaluate the model across `num_processes` processes
                 p.start()
                 processes.append(p)
 
             for p in processes:
                 p.join()
-            evaluation_logging(list(eval_logs), epoch , "valid")
+            evaluation_logging(list(eval_logs), epoch, "valid")
     end = time.time()
     print("Time : %.4f" % (end - begin))
 
     # Evaluation on test dataset
     eval_logs = []
     processes = []
-    eval_logs = torch.tensor([0.0 for _ in range(args.n_clients)]).share_memory_()
+    eval_logs = torch.tensor(
+        [0.0 for _ in range(args.n_clients)]).share_memory_()
     processes = []
     for c_id in range(args.n_clients):
         # Restore global weights to client's model
         clients[c_id].set_global_weights(server.get_global_weights())
-        p = mp.Process(target=clients[c_id].evaluation, args=(c_id, random_clients, eval_logs, "test"))
-        # We evaluate the model across `num_processes` processes
+        p = mp.Process(target=clients[c_id].evaluation, args=(
+            c_id, random_clients, eval_logs, "test"))
+        # Evaluate the model across `num_processes` processes
         p.start()
         processes.append(p)
 
     for p in processes:
         p.join()
-    evaluation_logging(list(eval_logs), epoch , "test")
-
-
+    evaluation_logging(list(eval_logs), epoch, "test")
