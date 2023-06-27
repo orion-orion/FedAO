61e1bc7327cbb1f439cfc8d444ad7ebb4068fe56
orion-orion <orion-orion@foxmail.com>
2023-06-27 17:40:57 +0800

    feat: add fedprox in pytorch

--- fed_pytorch/clients.py
+++ fed_pytorch/clients.py
@@ -1,37 +1,61 @@
-import numpy as np
+# -*- coding: utf-8 -*-
 import math
 import gc
 import logging
 import numpy as np
 import torch
 from torch.utils.data import DataLoader
+
+
 class Clients:
-    def __init__(self, model_fn, optimizer_fn, args, train_datasets, valid_datasets, test_datasets):
+    def __init__(self, model_fn, optimizer_fn, args, train_datasets,
+                 valid_datasets, test_datasets):
         self.device = "cuda:%s" % args.gpu if args.cuda else "cpu"
         self.model = model_fn().to(self.device)
         self.optimizer = optimizer_fn(self.model.parameters())
         self.n_clients = args.n_clients
-        
-        self.train_dataloaders = [DataLoader(train_datasets[c_id], batch_size=args.batch_size, shuffle=True) for c_id in range(args.n_clients)]
-        self.valid_dataloaders = [DataLoader(valid_datasets[c_id], batch_size=args.batch_size, shuffle=False) for c_id in range(args.n_clients)]
-        self.test_dataloaders = [DataLoader(test_datasets[c_id], batch_size=args.batch_size, shuffle=False) for c_id in range(args.n_clients)]
 
-        # 初始化各client的权重
-        client_n_samples_train = [len(train_datasets[c_id]) for c_id in range(args.n_clients)]
+        self.train_dataloaders = [DataLoader(train_datasets[c_id],
+                                             batch_size=args.batch_size,
+                                             shuffle=True)
+                                  for c_id in range(args.n_clients)]
+        self.valid_dataloaders = [DataLoader(valid_datasets[c_id],
+                                             batch_size=args.batch_size,
+                                             shuffle=False)
+                                  for c_id in range(args.n_clients)]
+        self.test_dataloaders = [DataLoader(test_datasets[c_id],
+                                            batch_size=args.batch_size,
+                                            shuffle=False)
+                                 for c_id in range(args.n_clients)]
+
+        # Compute the proportion of the samples of each client to all samples
+        client_n_samples_train = [len(train_datasets[c_id])
+                                  for c_id in range(args.n_clients)]
         samples_sum_train = sum(client_n_samples_train)
-        self.client_train_prop = [len(train_datasets[c_id])/samples_sum_train for c_id in range(args.n_clients)]
+        self.client_train_prop = [
+            len(train_datasets[c_id])/samples_sum_train
+            for c_id in range(args.n_clients)]
         if args.valid_frac > 0:
-            client_n_samples_valid = [len(valid_datasets[c_id]) for c_id in range(args.n_clients)]
+            client_n_samples_valid = [len(valid_datasets[c_id])
+                                      for c_id in range(args.n_clients)]
             samples_sum_valid = sum(client_n_samples_valid)
-            self.client_valid_prop = [len(valid_datasets[c_id])/samples_sum_valid for c_id in range(args.n_clients)]
-        client_n_samples_test = [len(test_datasets[c_id]) for c_id in range(args.n_clients)]
+            self.client_valid_prop = [len(valid_datasets[c_id])
+                                      / samples_sum_valid
+                                      for c_id in range(args.n_clients)]
+        client_n_samples_test = [len(test_datasets[c_id])
+                                 for c_id in range(args.n_clients)]
         samples_sum_test = sum(client_n_samples_test)
-        self.client_test_prop = [len(test_datasets[c_id])/samples_sum_test for c_id in range(args.n_clients)]
+        self.client_test_prop = [len(test_datasets[c_id]) / samples_sum_test
+                                 for c_id in range(args.n_clients)]
 
-    def train_epoch(self, c_id, epoch, args):
-        """
-            Train one client with its own data for one epoch
-            cid: Client id
+    def train_epoch(self, c_id, epoch, args, global_weights):
+        """Train one client with its own data for one epoch.
+
+        Args:
+            c_id: client ID.
+            epoch: current global epoch.
+            args: other parameters for training.
+            global_weights: global model weights used in `FedProx` method.
         """
         self.model.train()
         for _ in range(args.local_epochs):
@@ -39,23 +63,50 @@ class Clients:
             for x, y in self.train_dataloaders[c_id]:
                 x, y = x.to(self.device), y.to(self.device)
                 self.optimizer.zero_grad()
-                
-                # Pytorch自带softmax和默认取了mean
+
+                # The `CrossEntropyLoss` function in Pytorch will divide by
+                # the mini-batch size by default
                 l = torch.nn.CrossEntropyLoss()(self.model(x), y)
+                if args.fed_method == "FedProx":
+                    l += self.prox_reg(dict(self.model.named_parameters()),
+                                       global_weights, args.mu)
+
                 l.backward()
-                self.optimizer.step()  
-                
+                self.optimizer.step()
+
                 loss += l.item() * y.shape[0]
                 n_samples += y.shape[0]
 
             gc.collect()
-        logging.info('Global epoch {}/{} - client {} -  Training Loss: {:.3f}'.format(epoch, args.global_epochs, c_id, loss / n_samples))
+        logging.info("Global epoch {}/{} - client {} -  Training Loss: {:.3f}"
+                     .format(epoch, args.global_epochs, c_id,
+                             loss / n_samples))
         return n_samples
-    
-    def evaluation(self, c_id, mod = "valid"):    
-        if mod == "valid":
+
+    @ staticmethod
+    def flatten(params):
+        return torch.cat([param.flatten() for param in params])
+
+    def prox_reg(self, params1, params2, mu):
+        # The parameters returned by `named_parameters()` should be rearranged
+        # according to the keys of the parameters returned by `state_dict()`
+        params2_values = [params2[key] for key in params1.keys()]
+
+        # Multi-dimensional parameters should be flattened into one-dimensional
+        vec1 = self.flatten(params1.values())
+        vec2 = self.flatten(params2_values)
+        return mu / 2 * torch.norm(vec1 - vec2)**2
+
+    def evaluation(self, c_id, mode="valid"):
+        """Evaluation one client with its own data for one epoch.
+
+        Args:
+            c_id: client ID.
+            mode: choose valid or test mode.
+        """
+        if mode == "valid":
             dataloader = self.valid_dataloaders[c_id]
-        elif mod == "test":
+        elif mode == "test":
             dataloader = self.test_dataloaders[c_id]
 
         self.model.eval()
@@ -63,30 +114,37 @@ class Clients:
         with torch.no_grad():
             for x, y in dataloader:
                 x, y = x.to(self.device), y.to(self.device)
-                
+
                 logits = self.model(x)
                 pred = torch.argmax(logits.data, 1)
-                
+
                 correct += (pred == y).sum().item()
                 n_samples += y.shape[0]
 
         gc.collect()
         self.acc = correct/n_samples
         return {"ACC": self.acc}
-         
+
     def get_old_eval_log(self):
+        """Returns the evaluation result of the lastest epoch.
+        """
         return {"ACC": self.acc}
 
     def get_client_weights(self):
-        """ Return all of the weights list """
+        """Returns all of the weights in `OrderedDict` format. Note that
+        the `state_dict()` function returns the reference of the model
+        parameters. In consideration of computational efficiency, here we
+        choose to keep the reference.
+        """
         return self.model.state_dict()
 
     def set_global_weights(self, global_weights):
-        """ Assign all of the weights with global weights """
+        """Assigns all of the weights with global weights.
+        """
         self.model.load_state_dict(global_weights)
 
     def choose_clients(self, ratio=1.0):
-        """ randomly choose some clients """
+        """Randomly chooses some clients.
+        """
         choose_num = math.ceil(self.n_clients * ratio)
         return np.random.permutation(self.n_clients)[:choose_num]
-
--- fed_pytorch/fl.py
+++ fed_pytorch/fl.py
@@ -1,69 +1,76 @@
+# -*- coding: utf-8 -*-
 import logging
 from tqdm import tqdm
 
-def evaluation_logging(eval_logs, epoch, mod="valid"):
-    if mod == "valid":
-        logging.info('Global epoch%d Valid:' % epoch)
+
+def evaluation_logging(eval_logs, epoch, mode="valid"):
+    if mode == "valid":
+        logging.info("Global epoch%d Valid:" % epoch)
     else:
-        logging.info('Test:')
-        
+        logging.info("Test:")
+
     avg_eval_log = {}
     for key in eval_logs[0].keys():
         avg_eval_val = 0
         for i in range(len(eval_logs)):
             avg_eval_val += eval_logs[i][key]
         avg_eval_log[key] = avg_eval_val
-        
-    logging.info('ACC: ' + str(avg_eval_log["ACC"]))
-       
-       
+
+    logging.info("ACC: " + str(avg_eval_log["ACC"]))
+
+
 def run_fl(clients, args):
     global_weights = clients.get_client_weights()
-    # for c_id, train_data
     for epoch in range(1, args.global_epochs + 1):
-
         # We are going to sum up active clients' weights at each epoch
         client_weights_sum = None
-         
-        # participating_cids = server.select_clients(n_clients, opt['frac']) 
+
+        # Choose some clients that will train on this epoch
         random_clients = clients.choose_clients(args.frac)
-            
+
         # Train with these clients
         for c_id in tqdm(random_clients, ascii=True):
             # Restore global weights to client's model
             clients.set_global_weights(global_weights)
 
-            # train one client
-            clients.train_epoch(c_id, epoch, args)
+            # Train one client
+            clients.train_epoch(c_id, epoch, args, global_weights)
 
-            # obtain current client's weights
+            # Obtain current client's weights
             current_client_weights = clients.get_client_weights()
 
-            # sum it up with weights
+            # Sum it up with weights
             if client_weights_sum is None:
-                client_weights_sum = dict((key, value * clients.client_train_prop[c_id]) for key, value in current_client_weights.items())
+                client_weights_sum = dict(
+                    (key, value * clients.client_train_prop[c_id])
+                    for key, value in current_client_weights.items())
             else:
                 for key in client_weights_sum.keys():
-                    client_weights_sum[key] += clients.client_train_prop[c_id] * current_client_weights[key]
+                    client_weights_sum[key] += \
+                        clients.client_train_prop[c_id] \
+                        * current_client_weights[key]
 
-        # assign the avg weights to global weights
+        # Assign the avg weights to global weights
         global_weights = client_weights_sum
         if args.valid_frac > 0 and epoch % args.eval_interval == 0:
             clients.set_global_weights(global_weights)
             eval_logs = []
             for c_id in tqdm(range(args.n_clients), ascii=True):
                 if c_id in random_clients:
-                    eval_log = clients.evaluation(c_id, mod = "valid")
+                    eval_log = clients.evaluation(c_id, mode="valid")
                 else:
                     eval_log = clients.get_old_eval_log()
-                eval_logs.append(dict((key, value * clients.client_valid_prop[c_id]) for key, value in eval_log.items()))
+                eval_logs.append(dict((key, value
+                                       * clients.client_valid_prop[c_id])
+                                      for key, value in eval_log.items()))
 
-            evaluation_logging(eval_logs, epoch, mod="valid")
-   
+            evaluation_logging(eval_logs, epoch, mode="valid")
+
     clients.set_global_weights(global_weights)
     eval_logs = []
     for c_id in tqdm(range(args.n_clients), ascii=True):
-        eval_log = clients.evaluation(c_id, mod = "test")
-        eval_logs.append(dict((key, value * clients.client_test_prop[c_id]) for key, value in eval_log.items()))
+        eval_log = clients.evaluation(c_id, mode="test")
+        eval_logs.append(dict((key, value * clients.client_test_prop[c_id])
+                              for key, value in eval_log.items()))
 
-    evaluation_logging(eval_logs, epoch, mod="test")
+    evaluation_logging(eval_logs, epoch, mode="test")
